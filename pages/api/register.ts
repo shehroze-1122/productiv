adbaa298f2655a379ff1702c4a8ac13d937f2c1c
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2023-03-23 14:13:38 +0500

    seed a project with 3 tasks on register

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -3,6 +3,7 @@ import { serialize } from "cookie"
 import { db } from "@/lib/db"
 import { hashPassword } from "@/lib/auth"
 import { createJWT } from "@/lib/jwt"
+import { TASK_STATUS, User } from "@prisma/client"
 
 export default async function register(
   req: NextApiRequest,
@@ -26,6 +27,8 @@ export default async function register(
         password: hashedPassword
       }
     })
+
+    await seedProjectWithTasks(user)
     const jwt = await createJWT(user)
 
     res.setHeader(
@@ -40,4 +43,46 @@ export default async function register(
   } catch (error) {
     res.status(400).json({ error: "User already exists" })
   }
+}
+
+async function seedProjectWithTasks(user: User) {
+  const project = await db.project.create({
+    data: {
+      name: "Be Productive",
+      description: "Master Procrastinator's Guide to Productivity",
+      due: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+      ownerId: user.id
+    }
+  })
+
+  await db.task.createMany({
+    data: [
+      {
+        name: "Sign up on Productiv🚀",
+        ownerId: user.id,
+        projectId: project.id,
+        due: new Date(),
+        description: "Register on Productiv today!",
+        status: TASK_STATUS.COMPLETED
+      },
+      {
+        name: "Organize your closet",
+        ownerId: user.id,
+        projectId: project.id,
+        due: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+        description:
+          "Say goodbye to that pile of clothes you've been meaning to donate and hello to a closet that Marie Kondo would be proud of",
+        status: TASK_STATUS.NOT_STARTED
+      },
+      {
+        name: "Learn a new skill",
+        ownerId: user.id,
+        projectId: project.id,
+        due: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        description:
+          "Whether it's a language, a dance move, or a magic trick, challenge yourself to step out of your comfort zone and try something new",
+        status: TASK_STATUS.STARTED
+      }
+    ]
+  })
 }
