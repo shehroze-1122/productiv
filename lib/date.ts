ad2360f27bfeb6dbf58bad6874adb835e9f5d20b
shehroze-1122 <stalat.bee19seecs@seecs.edu.pk>
2023-03-23 13:08:19 +0500

    added helper to format date for input value

--- lib/date.ts
+++ lib/date.ts
@@ -5,3 +5,17 @@ export const formatDate = (date: Date) =>
     month: "short",
     day: "numeric"
   })
+
+export const formatDateForInput = (dateString: string) => {
+  const date = new Date(dateString)
+
+  const year = date.getFullYear()
+  const month =
+    date.getMonth() < 9
+      ? `0${date.getMonth() + 1}`
+      : (date.getMonth() + 1).toString()
+  const day =
+    date.getDate() < 10 ? `0${date.getDate()}` : date.getDate().toString()
+
+  return `${year}-${month}-${day}`
+}
